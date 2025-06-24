c7922004dc890e8216d465d0108574c03cced7d8
DevelopWithAP <apostolisparga@gmail.com>
2025-06-24 17:33:42 +0300

    Add loading and no found pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
-export default function Homepage() {
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function Homepage() {
+  await delay(3000);
   return (
     <div>
       <header>Store</header>
